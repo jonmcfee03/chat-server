aa6476595bed71728aaddfe4bf0c38536472ceef
Jonathan McFee <jmcfee@umich.edu>
2024-01-11 09:49:25 -0500

    Added mutexes for synchronization (idk why I didn't add those before lmao)

--- Network-Chat/server.cpp
+++ Network-Chat/server.cpp
@@ -11,10 +11,14 @@
 using namespace std;
 
 fd_set master;
+HANDLE mutex;
 
 DWORD WINAPI handle_connection(LPVOID p_socket);
 DWORD WINAPI handle_accept(LPVOID p_socket);
-DWORD WINAPI handle_send(LPVOID p_socket);
+DWORD WINAPI handle_send(LPVOID empty_param);
+void print_server() {
+	cout << "\rServer: ";
+}
 
 int main() {
 	cout << "========= SERVER ========== \n\n";
@@ -70,21 +74,34 @@ int main() {
 	}
 
 
+	mutex = CreateMutex(NULL, false, NULL);
+
+	if (mutex == NULL) {
+		cout << "Mutex create error: " << GetLastError() << endl;
+		return 1;
+	}
+
 	SOCKET* p_socket = new SOCKET;
 	*p_socket = serverSocket;
 	DWORD sendThreadId, acceptThreadId;
 	HANDLE sendHdl, acceptHdl;
-	sendHdl = CreateThread(NULL, 0, handle_send, p_socket, 0, &sendThreadId);
+	sendHdl = CreateThread(NULL, 0, handle_send, NULL, 0, &sendThreadId);
 	acceptHdl = CreateThread(NULL, 0, handle_accept, p_socket, 0, &acceptThreadId);
 
 	HANDLE hdls[2] = { sendHdl, acceptHdl };
 
-	WaitForMultipleObjects(2, hdls, 1, INFINITE);
+	WaitForMultipleObjects(2, hdls, 0, INFINITE);
 
 	delete p_socket;
 
+	for (int i = 0; i < master.fd_count; ++i) {
+		closesocket(master.fd_array[i]);
+		FD_CLR(master.fd_array[i], &master);
+	}
 	closesocket(serverSocket);
 	WSACleanup();
+
+	cout << "Server shutting down" << endl;
 	return 0;
 }
 
@@ -103,11 +120,14 @@ DWORD WINAPI handle_accept(LPVOID p_server_socket) {
 			*p_socket = acceptSocket;
 			DWORD threadId;
 			HANDLE hdl;
+			
+			WaitForSingleObject(mutex, INFINITE);
+			FD_SET(acceptSocket, &master);
+			ReleaseMutex(mutex);
+
 			hdl = CreateThread(NULL, 0, handle_connection, p_socket, 0, &threadId);
 			p_socket = NULL;
-			FD_SET(acceptSocket, &master);
 		}
-
 	}
 }
 
@@ -124,57 +144,74 @@ DWORD WINAPI handle_connection(LPVOID p_socket) {
 	string welcomeMessage = "\rServer: Welcome to the server ";
 	welcomeMessage = welcomeMessage + clientUsername;
 
+	WaitForSingleObject(mutex, INFINITE);
 	for (int i = 0; i < master.fd_count; ++i) {
 		send(master.fd_array[i], welcomeMessage.c_str(), strlen(welcomeMessage.c_str()), 0);
 	}
+	ReleaseMutex(mutex);
 
-	std::cout << '\r';
-	std::cout << clientUsername << " connected" << endl;
-	std::cout << "Server: ";
+	std::cout << '\r' << clientUsername << " connected" << endl;
+	print_server();
 
 	while (true) {
 		int byteCount = recv(acceptSocket, buffer, 200, 0);
-		buffer[byteCount] = '\0';
-		if (byteCount == SOCKET_ERROR) {
-			cout << "\rServer receive error: " << WSAGetLastError() << endl;
+		if (byteCount <= 0) {
+			WaitForSingleObject(mutex, INFINITE);
+			closesocket(acceptSocket);
 			FD_CLR(acceptSocket, &master);
-			return 0;
-		}
-		else if (byteCount == 0) {
-			cout << "\rA client disconnected" << endl;
-			cout << "Server: ";
-			return 0;
-		}
-		else {
-			cout << '\r' << buffer << endl;
-			cout << "\rServer: ";
+			ReleaseMutex(mutex);
 			
-			if (!strcmp(buffer, "/quit")) {
-				return 0;
+			//if the client disconnects
+			if (byteCount == 0) {
+				cout << "\rA client disconnected" << endl;
 			}
-
-			//print out message for all others
-			for (int i = 0; i < master.fd_count; ++i) {
-				if (master.fd_array[i] != acceptSocket)
-					send(master.fd_array[i], buffer, byteCount, 0);
+			//if there is an error with the client
+			else {
+				cout << "\rServer receive error: " << WSAGetLastError() << endl;
 			}
+			print_server();
+			return 0;
 		}
+		
+		WaitForSingleObject(mutex, INFINITE);
+		buffer[byteCount] = '\0';
+		cout << '\r' << buffer << endl;
+		print_server();
+		ReleaseMutex(mutex);
+
+		if (!strcmp(buffer, "/quit")) {
+			WaitForSingleObject(mutex, INFINITE);
+			closesocket(acceptSocket);
+			FD_CLR(acceptSocket, &master);
+			ReleaseMutex(mutex);
+			return 0;
+		}
+
+		//print out message for all others
+		WaitForSingleObject(mutex, INFINITE);
+		for (int i = 0; i < master.fd_count; ++i) {
+			if (master.fd_array[i] != acceptSocket)
+				send(master.fd_array[i], buffer, byteCount, 0);
+		}
+		ReleaseMutex(mutex);
 	}
 	return 0;
 }
 
-DWORD WINAPI handle_send(LPVOID p_socket) {
-	SOCKET sendSocket = *((SOCKET*)p_socket);
+DWORD WINAPI handle_send(LPVOID empty_param) {
 	string data = "";
+	int byteCount;
 	while (true) {
 		std::getline(std::cin, data);
 		if (data.size() > 0) {
 			data = "Server: " + data;
+			WaitForSingleObject(mutex, INFINITE);
 			for (int i = 0; i < master.fd_count; ++i) {
-				send(master.fd_array[i], data.c_str(), strlen(data.c_str()), 0);
+				byteCount = send(master.fd_array[i], data.c_str(), strlen(data.c_str()), 0);
 			}
+			print_server();
+			ReleaseMutex(mutex);
 		}
-		cout << "Server: ";
 	}
 	return 0;
 }
